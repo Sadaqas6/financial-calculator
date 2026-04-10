22b45742e11d74c9ef54668b044b152507aaa9ab
sadaqasalaam <sadaqasalaam6@gmail.com>
2026-04-10 13:53:21 -0400

    initial commit

--- src/main/java/MortgageCalculator.java
+++ src/main/java/MortgageCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FinancialCalculator {
+public class MortgageCalculator {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -8,27 +8,31 @@ public class FinancialCalculator {
 
 
         System.out.println("------- Mortgage Calculator -------");
+        System.out.println("Lets calculate your monthly payment and interest! ");
+        System.out.println();
+        System.out.print("Principal: ");
+        double p = sc.nextDouble();
 
-        System.out.print("\tPrincipal: ");
-        double principal = sc.nextDouble();
+        System.out.print("Annual Interest Rate: ");
+        double i = sc.nextDouble() /100;
 
-        System.out.print("\tAnnual Interest: ");
-        double interestRate = sc.nextDouble();
-
-        System.out.print("\tLoan Length(in years): ");
+        System.out.print("Loan Length(in years): ");
         int loanLength = sc.nextInt();
 
+        double n = 12 * loanLength;
 
-
-
+        i /= 12; // interest divided by month  r divided 12
 
 
         // M = P × (i × (1+i)^n) / ((1+i)^n - 1)
 
+        double m = p * (i * Math.pow(1 + i, n )) / (Math.pow(1 + i, n ) - 1);
 
+        System.out.printf("Monthy Payments: %.2f%n " , m);
 
+        double totalInterest = (m * n)-p;
 
-
+        System.out.printf("\n$%.2f/mo payment with a total interest of $%.2f", m , totalInterest);
 
 
 
